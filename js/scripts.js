19108f4b0f5c052272c1b779e83ff5e1a83dd879
VyacheslavVolkhin <vv@htmlworker.com>
2025-07-30 11:09:35 +0500

    add

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@ window.addEventListener('load', function() {
 
 
 	//loaded
-	document.body.classList.add('loaded');
 	setTimeout(function() {
+		document.body.classList.add('loaded');
 	}, 1000);
 
 
@@ -546,7 +546,7 @@ document.addEventListener("DOMContentLoaded", function() {
 	});
 	
 
-	
+
 
 	// gallery and values sliders sync
 	const gallerySwipers = [];
